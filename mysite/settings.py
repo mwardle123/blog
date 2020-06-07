07933b8856d05dda20c3eedf241356a431a78745
Matthew Wardle <mwardle1000@gmail.com>
2020-06-07 19:54:52 +0100

    Security bug fix.

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = ')2pnvzb$*)e8!1skcreo4%p+3ykm7ebl@v0u36wuj)3#r$(6rj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
