a6722882db2245216314547371d830ff93e1ba44
Matthew Wardle <mwardle1000@gmail.com>
2020-06-17 20:16:09 +0100

    Added functional tests to check multiple rows could be added correctly.

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_cv_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_cv_and_retrieve_it_later(self):
         # Checking out homepage
         self.browser.get('http://127.0.0.1:8000')
@@ -39,16 +44,17 @@ class NewVisitorTest(unittest.TestCase):
         # When hitting enter, the page updates, and now the page lists the new something
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_cv_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Something', [row.text for row in rows])
-        self.assertIn('2: Something else', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Something')
             
         # There is still a text box to add another item. Enter something else
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Something else')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on the list
+        self.check_for_row_in_list_table('1: Something')
+        self.check_for_row_in_list_table('2: Something else')
 
         # The site has generated a unique URL and it explains thos
 
