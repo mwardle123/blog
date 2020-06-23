5ec953cac0d57326674d5d08e72acfdbfba411d1
Matthew Wardle <mwardle1000@gmail.com>
2020-06-23 15:36:08 +0100

    Refactored functional tests to remove voodoo sleeps.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -11,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_cv_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element_by_id('id_cv_table')  
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException) as e:  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise e  
+                time.sleep(0.5)
 
     def test_can_start_cv_and_retrieve_it_later(self):
         # Checking out homepage
@@ -43,19 +54,19 @@ class NewVisitorTest(LiveServerTestCase):
 
         # When hitting enter, the page updates, and now the page lists the new something
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Something')
+        self.wait_for_row_in_list_table('1: Something')
             
         # There is still a text box to add another item. Enter something else
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Something else')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on the list
-        self.check_for_row_in_list_table('1: Something')
-        self.check_for_row_in_list_table('2: Something else')
+        self.wait_for_row_in_list_table('1: Something')
+        self.wait_for_row_in_list_table('2: Something else')
 
         # The site has generated a unique URL and it explains this
 
         # Visit that URL to see CV is still there.
+
+        
