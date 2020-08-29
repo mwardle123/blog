2afcea3b6e6c7977697158118f8aa5818f1404cc
Matthew Wardle <mwardle1000@gmail.com>
2020-08-29 18:41:51 +0100

    Updated admin options.

--- apps/cv/admin.py
+++ apps/cv/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import  CV, Category
+from .models import  CV, Category, Item
 
 admin.site.register(CV)
 admin.site.register(Category)
+admin.site.register(Item)
